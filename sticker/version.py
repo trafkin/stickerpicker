fcd93eed685ce31f11cd2b43bb1c9cbcb652586f
Christopher Valerio <christopher@valerio.guru>
2024-10-08 22:57:32 -0600

    new stickers

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '333567f4'
+version = '0.1.0+dev.333567f4'
+linkified_version = '0.1.0+dev.[333567f4](https://github.com/maunium/stickerpicker/commit/333567f481e60443360aa7199d481e1a45b3a523)'
